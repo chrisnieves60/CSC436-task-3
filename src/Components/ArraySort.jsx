56f2b181fb81443313af9cdee88356c6701abf7d
chrisnieves60 <chrisnieves60@gmail.com>
2023-02-19 07:11:59 -0500

    hw-3

--- src/Components/ArraySort.jsx
+++ src/Components/ArraySort.jsx
@@ -2,14 +2,17 @@ import { products } from "../products";
 const ArraySort = () => {
 
 
-    const arrayOfPrices = products.map(({price}) => {
+    const arrayOfPrices = products.map(({price}) => {//object, brackets NEEDED in parameter for .map
         return price
     }).sort((a, b)=>a-b);
 
     console.log(arrayOfPrices)
 
-    return arrayOfPrices.map((price) => {
-        return <li>{price}</li>
+    
+    return arrayOfPrices.map((price) => { //array, no brackets needed in parameter for .map
+        return <div>
+           <li>{price}</li>
+            </div>
 })
 }
 
